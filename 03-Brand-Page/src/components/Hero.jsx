4362db0566ded6fa5e7da46f76b9dcfe3487c38c
Syed <amanxsyed@gmail.com>
2025-04-26 16:36:48 +0500

    Hero and Navigation components for improved layout and styling

--- 03-Brand-Page/src/components/Hero.jsx
+++ 03-Brand-Page/src/components/Hero.jsx
@@ -1,37 +1,37 @@
 const Hero = () => {
     return (
-      <main className="flex items-center h-[calc(100vh-72px)] justify-center mx-auto my-0 max-w-[1280px] px-32">
-        <div className="flex flex-col gap-9">
-          <h1 className="font-extrabold text-8xl leading-[102px]">
+      <main className="flex items-center h-[85vh] justify-center mx-auto max-w-[1280px] my-0 px-40">
+        <div className="flex flex-col mb-16 gap-4 mt-20">
+          <h1 className="font-extrabold text-8xl leading-[90px]">
             YOUR FEET DESERVE THE BEST
           </h1>
   
-          <p className="font-semibold text-[#2e2e2e] max-w-[400px]">
+          <p className="font-semibold text-[#2e2e2e] max-w-[400px] gap-6">
             YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
             SHOES. YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
             SHOES.
           </p>
   
-          <div className="flex gap-10">
-            <button className="bg-[#d01c28] text-white px-4 py-2 h-8 font-medium">
+          <div className="flex gap-10 items-center ">
+            <button className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 h-8 font-medium flex items-center rounded transition cursor-pointer">
               Shop Now
             </button>
-            <button className="text-[#2e2e2e] border border-gray-500 px-4 py-2 h-8 font-medium">
+            <button className="text-[#2e2e2e] border border-gray-500 px-4 py-2 h-8 font-medium flex items-center rounded transition cursor-pointer">
               Category
             </button>
           </div>
   
-          <div className="mt-6">
-            <p className="text-sm mb-2">Also Available On</p>
-            <div className="flex gap-4">
+          <div>
+            <p className="text-sm">Also Available On</p>
+            <div className="flex gap-4 mt-2">
               <img src="/images/daraz.svg" alt="daraz" className="w-12 h-8" />
               <img src="/images/telemart.png" alt="telemart" className="w-8 h-8" />
             </div>
           </div>
         </div>
   
-        <div className="flex justify-center items-center w-[100%]">
-          <img src="/images/hero-image.png" alt="Hero" className="w-full" />
+        <div className="flex justify-center items-center w-full h-full mb-0">
+          <img src="/images/hero-image.png" alt="Hero"    />
         </div>
       </main>
     );
--- 03-Brand-Page/src/components/Navigation.jsx
+++ 03-Brand-Page/src/components/Navigation.jsx
@@ -12,7 +12,7 @@ const Navigation = () => {
           <li><a href="#">Contact</a></li>
         </ul>
   
-        <button className="bg-red-500 text-white px-4 py-1 h-8 font-medium rounded hover:bg-red-600 transition">
+        <button className="bg-red-500 text-white px-4 py-1 h-8 font-medium rounded hover:bg-red-600 transition cursor-pointer">
           Login
         </button>
       </nav>
