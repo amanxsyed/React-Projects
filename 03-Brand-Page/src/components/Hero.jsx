240ffa913b83de962fb9bb7714c19c4c2dccf62c
Syed <amanxsyed@gmail.com>
2025-04-26 12:39:02 +0500

    Add daraz logo and update Hero component layout

--- 03-Brand-Page/src/components/Hero.jsx
+++ 03-Brand-Page/src/components/Hero.jsx
@@ -1,22 +1,22 @@
 const Hero = () => {
     return (
-      <main className="container max-w-[1280px] mx-auto flex items-center h-[calc(100vh-72px)]">
-        <div className="flex flex-col gap-9">
-          <h1 className="font-extrabold leading-[102px] text-[108px]">
+      <main className="flex ">
+        <div className="flex flex-col gap-6">
+          <h1 className="font-extrabold text-5xl">
             YOUR FEET DESERVE THE BEST
           </h1>
   
-          <p className="font-semibold text-gray-500 max-w-[400px]">
+          <p className="font-semibold text-[#2e2e2e] max-w-[400px]">
             YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
             SHOES. YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
             SHOES.
           </p>
   
           <div className="flex gap-10">
-            <button className="bg-red-500 text-white px-8 py-2 h-12 font-medium text-2xl rounded hover:bg-red-600 transition">
+            <button className="bg-[#d01c28] text-white px-4 py-2 h-8 font-medium">
               Shop Now
             </button>
-            <button className="border border-gray-500 text-gray-500 px-8 py-2 h-12 font-medium text-2xl rounded hover:bg-gray-100 transition">
+            <button className="text-[#2e2e2e] border border-gray-500 px-4 py-2 h-8 font-medium">
               Category
             </button>
           </div>
@@ -24,14 +24,14 @@ const Hero = () => {
           <div className="mt-6">
             <p className="text-sm mb-2">Also Available On</p>
             <div className="flex gap-4">
-              <img src="/images/amazon.png" alt="amazon-logo" className="w-24" />
-              <img src="/images/flipkart.png" alt="flipkart-logo" className="w-24" />
+              <img src="/images/daraz.svg" alt="daraz" className="w-24" />
+              <img src="/images/telemart.png" alt="telemart" className="w-24" />
             </div>
           </div>
         </div>
   
-        <div className="flex-1">
-          <img src="/images/hero-image.png" alt="hero-image" className="w-full" />
+        <div className="flex justify-center items-center w-full h-full">
+          <img src="/images/hero-image.png" alt="Hero" className="w-full" />
         </div>
       </main>
     );
